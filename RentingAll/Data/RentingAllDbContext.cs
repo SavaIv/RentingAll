b5bfcd812136cddfe904bb182059be835f68f17f
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-03-30 20:15:06 +0300

    MVC template and small refactoring of the template

---
+++ RentingAll/Data/RentingAllDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentingAll.Data
+{
+    public class RentingAllDbContext : IdentityDbContext
+    {
+        public RentingAllDbContext(DbContextOptions<RentingAllDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
