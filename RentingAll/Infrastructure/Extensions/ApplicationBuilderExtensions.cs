87526f1f563ba452b3ae04d7541e4832daedca70
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-04-11 15:11:38 +0300

    Seed Categories

--- RentingAll/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ RentingAll/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RentingAll.Areas.Cars.Data.Models;
 using RentingAll.Data;
 
 namespace RentingAll.Infrastructure.Extensions
@@ -13,7 +14,30 @@ namespace RentingAll.Infrastructure.Extensions
 
             data.Database.Migrate();
 
+            SeedCategories(data);
+
             return app;
+        }
+
+        private static void SeedCategories(RentingAllDbContext data)
+        {
+            if (data.Categories.Any())
+            {
+                return;
+            }
+
+            data.Categories.AddRange(new[]
+            {
+                new Category {Name = "Mini"},
+                new Category {Name = "Economy"},
+                new Category {Name = "Midsize"},
+                new Category {Name = "Large"},
+                new Category {Name = "SUV"},
+                new Category {Name = "Vans"},
+                new Category {Name = "Luxury"}                
+            });
+
+            data.SaveChanges();
         }
     }
 }
