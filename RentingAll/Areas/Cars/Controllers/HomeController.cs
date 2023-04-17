c0ad9a6fe569942c28aa4867d5c1850f5f94bf47
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-04-17 09:54:50 +0300

    Clean/modify the template html.

--- RentingAll/Areas/Cars/Controllers/HomeController.cs
+++ RentingAll/Areas/Cars/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentingAll.Models;
+using System.Diagnostics;
 
 namespace RentingAll.Areas.Cars.Controllers
 {
@@ -7,6 +9,12 @@ namespace RentingAll.Areas.Cars.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
