2b45db6db0841242c0791863f6cc83fa050c71aa
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-04-19 23:52:01 +0300

    AddCar view

---
+++ RentingAll/Areas/Cars/Controllers/CarsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentingAll.Areas.Cars.Models.Cars;
+
+namespace RentingAll.Areas.Cars.Controllers
+{
+    public class CarsController : AreaCarsBaseController
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel car) 
+        {
+            return View();
+        }
+    }
+}
---
+++ RentingAll/Areas/Cars/Models/Cars/AddCarFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentingAll.Areas.Cars.Models.Cars
+{
+    public class AddCarFormModel
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Year { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
